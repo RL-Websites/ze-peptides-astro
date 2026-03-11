81d205373071f8490bdfca832b6f9f0b3446094e
Iftekhar Hyder <iftekhar@redlimesolutions.com>
2026-03-11 15:35:27 +0600

    Package Product Card ui development

--- public/Themes/Thrivewellrx.Theme.ZePeptides/assets/js/tooltip.js
+++ public/Themes/Thrivewellrx.Theme.ZePeptides/assets/js/tooltip.js
@@ -32,4 +32,41 @@ $(document).ready(function () {
 	$(document).on("click", function () {
 		$(".statusTooltipBtn").tooltip("hide");
 	});
+
+	// product card tooltip
+	$(".productTooltipBtn").tooltip({
+		trigger: "manual",
+		placement: "right",
+		offset: [0, 10], // ✅ adds gap from the trigger element
+
+		html: true,
+		container: "body",
+		template:
+			'<div class="tooltip product-tooltip-wrapper" role="tooltip">' +
+			'<div class="tooltip-arrow"></div>' +
+			'<div class="tooltip-inner"></div>' +
+			"</div>",
+	});
+
+	// Toggle tooltip on button click
+	$(document).on("click", ".productTooltipBtn", function (e) {
+		e.stopPropagation();
+
+		var $this = $(this);
+
+		// Close others
+		$(".productTooltipBtn").not($this).tooltip("hide");
+
+		$this.tooltip("toggle");
+	});
+
+	// Allow interaction inside tooltip
+	$(document).on("click", ".product-tooltip-wrapper", function (e) {
+		e.stopPropagation();
+	});
+
+	// Close when clicking outside
+	$(document).on("click", function () {
+		$(".productTooltipBtn").tooltip("hide");
+	});
 });
